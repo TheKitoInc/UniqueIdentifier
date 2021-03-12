41446f589297f62950502b561059f032c02c0c8a
TheKito <11139894+TheKito@users.noreply.github.com>
2021-03-12 20:44:20 -0300

    Create UniqueIdentifier.php

---
+++ src/UniqueIdentifier.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kito\UniqueIdentifier;
+/**
+ * Description of UniqueIdentifier
+ *
+ * @author Instalacion
+ */
+class UniqueIdentifier {
+    //put your code here
+}
