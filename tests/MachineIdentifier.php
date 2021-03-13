539806a083d2a5c1cad38bbbe113bc515f5c38f4
TheKito <11139894+TheKito@users.noreply.github.com>
2021-03-12 21:06:37 -0300

    Create MachineIdentifier.php

---
+++ tests/MachineIdentifier.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class MachineIdentifierTest extends TestCase
+{
+    public function testFileExists(): string
+    {
+        $path = __DIR__.'/../src/MachineIdentifier.php';
+        $this->assertFileExists($path);
+
+        return $path;
+    }
+
+    /**
+     * @depends testFileExists
+     */
+    public function testClassExists(string $path): void
+    {
+        include_once $path;
+        $this->assertTrue(class_exists(Kito\UniqueIdentifier\MachineIdentifier::class));
+    }
+
+    /**
+     * @depends testClassExists
+     */
+    public function testGetLinuxMachineID()
+    {
+        $lmid = Kito\UniqueIdentifier\MachineIdentifier::getLinuxMachineID();
+        $this->assertIsNotBool($lmid);
+        $this->assertIsString($lmid);
+        $this->assertNotEmpty($lmid);        
+    }
+}
