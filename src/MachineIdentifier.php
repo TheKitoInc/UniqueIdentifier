41ed363084ef51d7d4131da9ce3d821eaa88f4e1
TheKito <11139894+TheKito@users.noreply.github.com>
2021-03-12 21:06:32 -0300

    Create MachineIdentifier.php

---
+++ src/MachineIdentifier.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kito\UniqueIdentifier;
+
+/**
+ * Description of UniqueIdentifier.
+ *
+ * @author Instalacion
+ */
+class MachineIdentifier
+{
+    const linuxMachineIDPath = '/etc/machine-id';
+    
+    public static function getLinuxMachineID(): ?string
+    {
+        if(!file_exists(self::linuxMachineIDPath))
+        {
+            return null;
+        }
+            
+        if(!is_readable(self::linuxMachineIDPath))
+        {
+            return null;
+        }
+        
+        return file_get_contents(linuxMachineIDPath);
+    }
+}
